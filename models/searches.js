b540ba4ec4bf926afc49043ea047b997c63f8cac
Andres Acosta <andresakosta@outlook.com>
2021-04-10 15:32:40 -0500

    Creating a instance of axios

--- models/searches.js
+++ models/searches.js
@@ -1,11 +1,25 @@
 const { default: axios } = require("axios");
 
-class Serches{
-    constructor(){
+
+class Serches {
+
+    constructor() {
         //Read DB
     }
-    async city(place= ''){
-        const res = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Bogota.json?access_token=pk.eyJ1IjoiYW5kcmV3YWtvc3RhIiwiYSI6ImNrbmMyamZjaTB6amoyeG9oOHd0N2xiZTQifQ.NOgpg4o9phJCoqLonketjg&cachebuster=1618079405279&autocomplete=true&limit=5&language=es')
+    get paramsMapbox() {
+        return {
+            'access_token': 'pk.eyJ1IjoiYW5kcmV3YWtvc3RhIiwiYSI6ImNrbmMyamZjaTB6amoyeG9oOHd0N2xiZTQifQ.NOgpg4o9phJCoqLonketjg',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+    async city(place = '') {
+        //const res = await axios.get('?access_token=pk.eyJ1IjoiYW5kcmV3YWtvc3RhIiwiYSI6ImNrbmMyamZjaTB6amoyeG9oOHd0N2xiZTQifQ.NOgpg4o9phJCoqLonketjg&cachebuster=1618079405279&autocomplete=true&limit=5&language=es')
+        const instance = axios.create({
+            baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json`,
+            params: this.paramsMapbox
+        })
+        const res = await instance.get();
         console.log(res.data);
         return true;
     }
